16ce0b76f1e1452c6a4596871dd85de9d0087502
bennami <benbenimane@hotmail.com>
2020-06-26 12:21:26 +0200

    created db and table

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,18 +1,30 @@
 <?php
     namespace App\Controller;
 
+    // if you are using composer, just use this
+
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Gufy\PdfToHtml;
 
 
     class ArticleController extends AbstractController{
+
+
+
+
+
         /**
          * @Route(path="/", methods={"GET"})
          */
         public function index(){
-
+            // initiate
+            $pdf = new PdfToHtml\Pdf('public/assets/pdftest.pdf');
+            $html = $pdf->html();
+            $page = $pdf->html(3);
             $articles =['art1','art2'];
-            return $this->render('articles/index.html.twig', array('articles' =>$articles));
+            return $this->render('articles/index.html.twig', array('articles' =>$page));
         }
     }
