2375918dc7c4317fb5af75cc1174fefc54f56e94
bennami <benbenimane@hotmail.com>
2020-07-05 00:04:25 +0200

    pusshing to db, getting records and reversing htmlentitle

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,15 +1,12 @@
 <?php
     namespace App\Controller;
 
-    // if you are using composer, just use this
-    //include 'vendor/autoload.php';
-
     use App\Entity\Articles;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\Finder\Finder;
-    use Gufy\PdfToHtml;
+    use Symfony\Component\DomCrawler\Crawler;
+
 
 
     class ArticleController extends AbstractController{
@@ -18,48 +15,55 @@
          * @Route(path="/", methods={"GET"})
          */
         public function index(){
-            //get all folders
-             $glob = glob("joachim-articles-html/*");
-                //print_r($glob);
-            foreach ($glob as $filename) {
+
+            //get all folders that contain html articles
+            $glob = glob("joachim-articles-html/*");
+            foreach ($glob as $folder) {
                 //for every folder, get html article
-                $glob2 = glob("${filename}/*html");
-                var_dump($filename);
+                $glob2 = glob("${folder}/*html");
+
                 //for every html, get its content and translate html entities to push to db
                 foreach ($glob2 as $html){
-                   var_dump( "$html"."\n");
+                    //get content of html file
                     $htmlContent = file_get_contents($html);
-                    $htmltag = preg_match( 'h1',$htmlContent);
-                    var_dump($htmltag);
+                    $htmlforDB = htmlentities($htmlContent);
 
-                  var_dump(htmlentities($htmlContent));
+                    //get title of article
+                    $crawler2 = new Crawler($htmlContent);
+                    $crawler2 = $crawler2->filter('head > title')->text();
+                    var_dump($crawler2);
+
+
+
+                    //save to db
+                    //$this->save($crawler2,$htmlforDB);
 
                 }
             }
-            //$finder = new Finder();
-            //$finder->files()->in("joachim-articles-html");
+            //get all articles
+            $entityManager = $this->getDoctrine()->getManager();
+            $product = $entityManager->getRepository('App:Articles');
+            $articles= $product->findAll();
 
-            //foreach ($finder as $file) {
-              //  $absoluteFilePath = $file->getRealPath();
-               // $fileNameWithExtension = $file->getRelativePathname();
-
-               //var_dump($absoluteFilePath,$fileNameWithExtension);
-            //}
-
-            $articles =['art1','art2','art3'];
-            return $this->render('articles/index.html.twig', array('articles' =>$articles));
+            //convert back to html tags before displaying
+            foreach ($articles as $article){
+                $body =[];
+              array_push($body, html_entity_decode($article->getBody())) ;
+            }
+            return $this->render('articles/index.html.twig', array('articles' =>$articles, 'bodyText'=> $body));
         }
 
         /**
          * @Route (path="/article/save", methods={"GET"})
          */
-        public function save(){
+        public function save($title,$html){
             $entityManager = $this->getDoctrine()->getManager();
             $article = new Articles();
-            $article->setTitle("first article");
-            $article->setBody("body text");
+            $article->setTitle($title);
+            $article->setBody($html);
             $entityManager->persist($article);
             $entityManager->flush();
+
             return new Response('saves article with id of'.$article->getId());
         }
 
@@ -69,4 +73,9 @@
         public function about(){
             return $this->render('about/index.html.twig');
         }
+
+        /**
+         * @Route (path="/article/id", methods={"GET"})
+         */
+
     }
